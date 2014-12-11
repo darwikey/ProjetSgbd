6fcc13b98cd6a4a3df545737c00a017374445f5c
AlexandrePhilippi <aphilippi@enseirb.fr>
2014-12-11 21:59:26 +0100

    Jointure feuille

--- src/Feuille.php
+++ src/Feuille.php
@@ -9,30 +9,33 @@ class Feuille
     $r = '<ul>';
 		
     $q1 = Database::query('
-		Select c.Nom as Nom_Club,
-		e0.Categorie,
-		m.Nom,
-		m.Prenom,
-		r0.Points,
-		r0.Fautes, 
+		Select c.Nom as Nom_Club, e.Categorie, m.Nom, m.Prenom, r0.Points, r0.Fautes, 
+
 		r0.Points = (Select max(Points) 
 			From Rencontrer r
 			Where r.ID_Rencontre = r0.ID_Rencontre
-			and ID_Equipe = r0.ID_Equipe
+			and r.ID_Equipe = r0.ID_Equipe
 			) as MeilleurJoueur,
+
 		r0.Fautes = (Select max(Fautes) 
 			From Rencontrer r
 			Where r.ID_Rencontre = r0.ID_Rencontre
-			and ID_Equipe = r0.ID_Equipe
+			and r.ID_Equipe = r0.ID_Equipe
 			) as PireJoueur 
 								
+		From (
+              (
+               (
+                (Rencontrer r0 inner join Equipe e
+                 On e.ID_Equipe = r0.ID_Equipe) 
+                inner join Club c            
+                On e.ID_Club = c.ID_Club) 
+               inner join Membre m
+               On r0.ID_Membre = m.ID_Membre) 
+              inner join Joueur j
+              On m.ID_Membre = j.ID_Membre)
 
-		From Rencontrer r0, Equipe e0, Club c, Membre m, Joueur j
-		Where ID_Rencontre = ' . $id_rencontre . '
-		and e0.ID_Equipe = r0.ID_Equipe
-		and e0.ID_Club = c.ID_Club
-		and r0.ID_Membre = m.ID_Membre
-		and m.ID_Membre = j.ID_Membre
+		Where r0.ID_Rencontre = ' . $id_rencontre . '
 
 		Order by Nom_Club, Points DESC, Fautes ASC');
 		
